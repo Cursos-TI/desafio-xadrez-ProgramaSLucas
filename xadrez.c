6fd32590018782c64831601fd1d9e5999b3cefa3
ProgramaSLucas <silviolucas2022@gmail.com>
2025-05-28 22:35:31 +0000

    atualizado

--- xadrez.c
+++ xadrez.c
@@ -1,30 +1,42 @@
+ #include <stdio.h>
+ 
  int main() {
+         
     // Mover o torre cinco casas para direita
     for (int i = 0; i < 5; i++) {
         printf("Direita\n"); //Imprime a direção do movimento
     }
 
-    int i = 1; //Mover o bispo cinco casas para cima
+    int i = 1; //Mover o bispo cinco casas para esquerda
 
     while (i <= 5) {
-        printf("Cima\n"); i++; //Imprime a direção do movimento
+        printf("Esquerda\n"); i++; //Imprime a direção do movimento
     }
     
     
-    int numero;  
+    int numero; 
     do {
-        printf("Digite um numero par para movimentar a rainha à esquerda");
+        printf("Digite um numero par para movimentar a rainha para frente");
         scanf("%d", &numero);
 
         if(numero % 2 == 0){
-            printf("%d esquerda!", numero);
+            printf("%d Frente!", numero);
         } else{
-           printf("%d cima!", numero);
+           printf("%d baixo!", numero);
         }
 
     } while (numero % 2 != 0);
-        printf("Você digitou um numero par, e moveu a rainha para esquerda");
+        printf("Você digitou um numero par, e moveu a rainha para frente\n");
+         
+        int movimentoCompleto = 1; //Flag para controlar o movimento em 'L'
 
+    while (movimentoCompleto--)
+      {
+        for (int i = 0; i < 2; i++) {
+         printf("Cima\n"); //Imprime "Cima" duas vezes
+        }
+        printf("Direita\n"); //Imprime "Direita" uma vez
+    } 
     
     return 0;
     }
