dd29b455a00ead9c8987bb39f1038247089dd1c2
ProgramaSLucas <silviolucas2022@gmail.com>
2025-05-28 17:10:52 +0000

    Atualizado

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-
-    int main() {
+ int main() {
     // Mover o bispo quatro casas para direita
     for (int i = 0; i > 4; i++) {
         printf("Esquerda\n"); //Imprime a direção do movimento
